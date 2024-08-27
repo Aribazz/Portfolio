e6b30ef2aa10323fb8963533a3db1ab6a6786609
Ariyo <ariyoaribass68@gmail.com>
2024-08-27 17:47:31 -0400

    First Commit Project work

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Public/*.html"],
+  theme: {
+    screens:{
+      sm:"450px",
+      md:"650px",
+      lg:"750px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
